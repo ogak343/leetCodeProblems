90fcf3103e8da0856c022ff37f0bdcaa3178850a
Ogabek <khamdamov343@gmail.com>
2023-12-25 17:29:09 +0500

    reverseInteger

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,18 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello there");
+        System.out.println(reverse(1534236469));
+    }
+
+    public static int reverse(int x) {
+        int r = 0;
+        while (x != 0) {
+            if(r < -214748364 || r > 214748364) {
+                return 0;
+            }
+            r = r * 10 + x % 10;
+            x = x / 10;
+        }
+        return r;
     }
 }
