b8cc35e7e0df6c02e41dcbf4bb4ec1f9deed2486
Ogabek <khamdamov343@gmail.com>
2023-12-25 17:21:21 +0500

    IntegerToRoman

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,27 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello there");
+        System.out.println(removeDuplicates(new int[]{1,1,2}));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+
+        int d = 0;
+        for(int i = 0; i < nums.length - 1; i++) {
+            if(nums[i]==nums[i+1]) {
+                d++;
+                nums[i] = 101;
+            }
+        }
+        int[] res = new int[nums.length - d];
+        d = 0;
+        for (int num : nums) {
+            if (num < 101) {
+                res[d] = num;
+                d++;
+            }
+        }
+        return nums.length;
     }
 }
+
