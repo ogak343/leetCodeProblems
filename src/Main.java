1f8c936ca8c01850599766d6ad3f2144ee202e79
Ogabek <khamdamov343@gmail.com>
2023-12-26 12:54:16 +0500

    reverseInteger

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,32 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello there");
+        rotate(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+    }
+
+    public static void rotate(int[][] matrix) {
+
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int var = matrix[i][j];
+                matrix[i][j] = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = var;
+            }
+        }
+        if (n % 2 == 1) {
+            int j = n / 2;
+            for (int i = 0; i < n / 2; i++) {
+                int var = matrix[i][j];
+                matrix[i][j] = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = var;
+            }
+        }
+
+        System.out.println("hello");
     }
 }
