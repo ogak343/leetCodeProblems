421df4e2118055bbc9e3d0dff682bdb1ab425b2e
Ogabek <khamdamov343@gmail.com>
2023-12-27 10:25:11 +0500

    reverseInteger

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,37 @@
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello there");
+        System.out.println(jump(new int[]{2, 3, 1}));
     }
+
+    public static int jump(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int pivot = 0;
+        int recurse = 0;
+        while (pivot < nums.length - 1) {
+            int max = 0;
+            int val = nums[pivot];
+            while (pivot + val >= nums.length) {
+                val--;
+            }
+            int offset = 0;
+            for (int i = val; i > 0; i--) {
+                if ((nums[pivot + i] + i > max) && (i + pivot < nums.length)) {
+                    max = nums[i + pivot] + i;
+                    offset = i;
+                }
+                if (pivot + offset == nums.length - 1) {
+                    break;
+                }
+            }
+            pivot += offset;
+            recurse++;
+        }
+        return recurse;
+    }
+
 }
