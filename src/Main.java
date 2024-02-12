e3f6381625cecd4a3aad654d83a561e8ce3c63f8
ogabek <khamdamov343@gmail.com>
2024-02-12 14:03:17 +0500

    done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Hello there");
+        System.out.println(maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
     }
+
+    public static int maxArea(int[] height) {
+
+        int l = 0;
+        int r = height.length -1;
+        int area = 0;
+
+        while (l < r)
+        {
+            area = Math.max(area, Math.min(height[l], height[r]) * (r - l));
+
+            if (height[l] < height[r])
+                l += 1;
+
+            else
+                r -= 1;
+        }
+        return area;
+    }
+
 }
