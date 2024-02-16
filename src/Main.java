07f4c8be48523efa425386b2365554e4479f714a
ogabek <khamdamov343@gmail.com>
2024-02-16 13:36:42 +0500

    done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello there");
+        System.out.println(myAtoi("-91283472332"));
+    }
+
+    public static int myAtoi(String s) {
+        boolean negative = s.contains("-");
+        s = s.replaceAll(" ", "");
+        s = s.replaceAll("-", "");
+        String updated = s.replaceAll("\\D.*", "");
+        if (updated.isEmpty()) {
+            return 0;
+        }
+
+        long result = Long.parseLong(updated);
+        if (negative && result > 2147483648L) {
+            result = 2147483648L;
+        }
+        if (!negative && result > 2147483647L) {
+            result = 2147483647L;
+        }
+        if(negative) {
+            return (int) -result;
+        } else{
+            return (int) result;
+        }
     }
 }
